dc4ebad206e1c28bb74e64c7d0e622420e5ae4e2
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2024-09-23 10:57:32 +0200

    aggiunte caricamento del file

--- Aziende/MainWindow.xaml.cs
+++ Aziende/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aziende.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,13 @@ namespace Aziende
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileInteraction fl = new FileInteraction("food");
+            fl.Load();
+            trvAziende.Text = fl.Content;
+        }
+
     }
 }
---
+++ Aziende/Models/FileInteraction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Aziende.Models
+{
+    internal class FileInteraction
+    {
+        public string FileName { get; private set; }
+        public string Content { get; private set; }
+        
+        //XmlDocument XmlDocument { get; set; }
+        public FileInteraction(string name)
+        {
+            FileName = name;
+            //XmlDocument = new XmlDocument();
+        }
+
+        public void Load()
+        {
+            StreamReader streamReader = new StreamReader($"{FileName}.xml");
+            Content = streamReader.ReadToEnd();
+        }  
+        public void Load(string name)
+        {
+            FileName = name;
+            Load();
+        }
+
+
+
+        
+    }
+}
